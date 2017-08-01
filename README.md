6fbff64ab94bf108da12bf48769a47632c433eac
Thomas Grice <Thomas Grice>
2017-08-01 09:14:09 -0700

    go returns json now :)

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # System Peformance Viewer
 ## a react-d3 / golang exercise
+displays fake system data across timescales (1 hr, 1 week)
+golang server serves up packaged file
 seed used: https://github.com/davezuko/react-redux-starter-kit
 modifications from that project:
 * use golang app server instead of node app server
 * redux-saga instead of redux-thunk (not fully removed)
 
-# requirements
+# system package requirements
 * golang
+* node/npm
 * yarn
 
 # how-to
-## install golang pkgs
-cd $GOPATH
-go get github.com/julienschmidt/httprouter
-## `yarn build` (must build before serving)
-## `yarn goserve` to start (nav localhost:3000)
-## `yarn test` to test
+1. `yarn install` install yarn pkgs
+2. install golang pkgs
+* cd $GOPATH
+* go get github.com/julienschmidt/httprouter
+3. `yarn build` (must build before serving)
+4. `yarn goserve` to start (nav localhost:3000)
+5. `yarn test` to test
 
 # design considerations
 color scheme: https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/18319153_795996577233857_534382761112645666_o.jpg?oh=ddd588c898c5613ba16e7fab5f3f3b7b&oe=5A07E89B
--- goServer.go
+++ goServer.go
@@ -4,37 +4,69 @@ import (
   "log"
   "fmt"
   "net/http"
-  "math/rand"
   "github.com/julienschmidt/httprouter"
+  "strings"
+  "strconv"
 )
 
-func genHardDriveSpace() {
+func intArrayToString(a []int) string {
+  b := make([]string, len(a))
+  for i, v := range a {
+    b[i] = strconv.Itoa(v)
+  }
+  return strings.Join(b, ",")
+}
+
+// { range: [min, max], static_name: "", unit: "", data: []}
+func genHardDriveSpace() string {
   // max 512gb, min 0gb
-  // slowly decreasing amount like [500, 498, 496, 493, 491, 489, 487, 485]
+  the_range :=[]int{0, 512}
+  data := []int{500, 498, 496, 493, 491, 489, 487, 485}
+  output := intArrayToString(data)
+  json := fmt.Sprintf("{range:[%v,%v],static_name:\"HARD_DRIVE_SPACE\",unit:\"gb\",data:[%v]}",the_range[0],the_range[1],output)
+  return json
 }
 
-func genNetworkUtilization() {
+func genNetworkUtilization() string {
   // max 11mbps, min 0mbps
-  // spike around [11, 5, 8, 2, 3, 6, 8, 4]
+  the_range :=[]int{0, 11}
+  data := []int{11, 5, 8, 2, 3, 6, 8, 4}
+  output := intArrayToString(data)
+  json := fmt.Sprintf("{range:[%v,%v],static_name:\"NETWORK_UTILIZATION\",unit:\"mbps\",data:[%v]}",the_range[0],the_range[1],output)
+  return json
 }
 
-func genMemoryUtilization() {
+func genMemoryUtilization() string {
   // max 16gb, min 0gb
-  // [16, 14, 12, 14, 12, 13, 12, 16]
+  the_range := []int{0, 16}
+  data := []int{16, 14, 12, 14, 12, 13, 12, 16}
+  output := intArrayToString(data)
+  json := fmt.Sprintf("{range:[%v,%v],static_name:\"MEMORY_UTILIZATION\",unit:\"gb\",data:[%v]}",the_range[0],the_range[1],output)
+  return json
 }
 
-func genCPUUtilization() {
+func genCPUUtilization() string {
   // 0-100%
-  // spike around [100, 44, 77, 33, 22, 11, 44, 55]
+  the_range := []int{0, 100}
+  data := []int{100, 44, 77, 33, 22, 11, 44, 55}
+  output := intArrayToString(data)
+  json := fmt.Sprintf("{range:[%v,%v],static_name:\"CPU_UTILIZATION\",unit:\"percent\",data:[%v]}",the_range[0],the_range[1],output)
+  return json
 }
 
-func genFakeData() {
-
+func genFakeData() string {
+  hds := genHardDriveSpace()
+  nwu := genNetworkUtilization()
+  mu := genMemoryUtilization()
+  cpu := genCPUUtilization()
+  output := fmt.Sprintf("[%v, %v, %v, %v]", hds, nwu, mu, cpu)
+  return output
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
     w.Header().Set("Content-Type", "application/json")
-    fmt.Fprint(w, "{data: \"test\"}")
+    output := genFakeData()
+    fmt.Fprintf(w, "{data: %v}", output)
 } 
 
 func main() {
